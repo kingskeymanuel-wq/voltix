566eb6887783dbd98306c9c814c62085ce12b015
Manuel Kingskey <kingskeymanuel@gmail.com>
2025-08-08 20:12:31 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,20 +1,28 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getPerformance } from "firebase/performance";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  "projectId": "voltix-smart-ci",
+  "appId": "1:781467646402:web:50d4519f4344f03f063e5b",
+  "storageBucket": "voltix-smart-ci.firebasestorage.app",
+  "apiKey": "AIzaSyC0do8vpuylOoZQ1srVxfWiuvGP9UONQWU",
+  "authDomain": "voltix-smart-ci.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "781467646402"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Performance Monitoring and get a reference to the service
+// To start collecting performance data, you must go to the Firebase console
+// to enable and complete the setup for Performance Monitoring.
+const perf = getPerformance(app);
+
 
 export { app };
